30e8184616bc7171c29a664a6a8ba1f7dfc9ea2c
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-09 15:31:48 +0100

    fixes

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -4,7 +4,7 @@ const Config = {
     APP_DOMAIN : import.meta.env.VITE_APP_DOMAIN,
     SITENAME : import.meta.env.VITE_SITENAME,
     VITE_API_URL_LOCAL : import.meta.env.VITE_VITE_API_URL_LOCAL,
-    API_URL : import.meta.env.VITE_API_URL,
+    API_URL : import.meta.env.VITE_API_URL ?? "https://electric-bike-rental.onrender.com",
     COUNTRY : import.meta.env.VITE_COUNTRY,
     DEF_CURRENCY_CODE : import.meta.env.VITE_DEF_CURRENCY_CODE,
     COUNTRY_CODE : import.meta.env.VITE_COUNTRY_CODE,
