b2237f2eba8beebf229a48e8e4fd7d420866421a
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-18 11:55:52 +0100

    fixes

--- src/pages/bikes.jsx
+++ src/pages/bikes.jsx
@@ -11,7 +11,7 @@ export default function Bikes() {
     const [loading, setloading] = useState(true)
     const [cookie, setCookie, removeCookie] = useCookies("")
 
-    if(!cookie.user){
+    if (!cookie.user) {
         window.location.href = "/"
     }
 
@@ -30,6 +30,30 @@ export default function Bikes() {
             })
     }, [bikes])
 
+    const deleteBike = () => {
+        axios.post("/delete-bike")
+        .then(
+            res => {
+                console.log(res)
+            }
+        )
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    const EditBike = () => {
+        axios.post("/edit-bike")
+        .then(
+            res => {
+                console.log(res)
+            }
+        )
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="dashbord bikes bg-white">
             <div className="d-flex">
@@ -47,7 +71,7 @@ export default function Bikes() {
                         <div className="main_">
                             <div className="d-flex sec_head justify-content-between">
                                 <h4 className="fw-bold mt-3">Bike Inventory</h4>
-                                <button className="btn bg text-white mt-3 ft"><i class="fa-solid fa-plus"></i> Add New Bike</button>
+                                <button data-bs-toggle="modal" data-bs-target="#createBikeModal" className="btn bg text-white mt-3 ft"><i class="fa-solid fa-plus"></i> Add New Bike</button>
                             </div>
 
                             <div className="d-flex sec_head mt-4 mb-3 justify-content-between">
@@ -102,7 +126,16 @@ export default function Bikes() {
                                                         <td>{val._id}</td>
                                                         <td className=''><div className="bike_status"><div className="dot"></div>Active</div></td>
                                                         <td>none</td>
-                                                        <td><div className="table_act"><i class="fa-solid fa-ellipsis"></i></div></td>
+                                                        <td><div className="table_act">
+                                                            <div class="dropdown dpd">
+                                                            <i class="fa-solid fa-ellipsis" data-bs-toggle="dropdown"></i>
+                                                                <ul class="dropdown-menu dpd bg-dark">
+                                                                    <li onClick={deleteBike}><h5 class="dropdown-header mt-3 ft mb-3 btn text-white mx-2 btn-danger">Delete</h5></li>
+                                                                    <li><h5 class="dropdown-header ft mb-3 btn text-white mx-2 btn-primary">Edit</h5></li>
+                                                                    <li><h5 class="dropdown-header ft mb-3 btn text-white mx-2 btn-info">Change Status</h5></li>
+                                                                </ul>
+                                                            </div>
+                                                        </div></td>
                                                     </tr>)
                                                 })}
                                             </tbody>
@@ -114,6 +147,30 @@ export default function Bikes() {
 
                         </div>
                     }
+                </div>
+            </div>
+
+            <div class="modal fade" id="createBikeModal">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+
+
+                        <div class="modal-header">
+                            <h4 class="modal-title">Modal Heading</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+
+
+                        <div class="modal-body">
+                            Modal body..
+                        </div>
+
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
